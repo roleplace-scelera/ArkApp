44ed5df08eee256840b2a106c3f3cb9434d8bc2d
Rachel Petitto <rachel.petitto@newmarketinc.com>
2017-06-16 14:26:59 -0400

    Add project files.

---
+++ ArkApp.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArkApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
